03f1b5faf6a5583f0b33bc3910ae07bc5e9074f2
Michael Camacho <mikedev0431@gmail.com>
2023-03-01 21:11:11 -0800

    Modifed for smaller with on container

--- components/pages/Dashboard/SideBar/styled.ts
+++ components/pages/Dashboard/SideBar/styled.ts
@@ -6,7 +6,6 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
   background-color: #FFF;
-  filter: drop-shadow(0 12px 24px rgb(104 112 118 / 0.15)) drop-shadow(0 12px 14px rgb(104 112 118 / 0.1));
 `;
 
 Styled.Container = Container;
